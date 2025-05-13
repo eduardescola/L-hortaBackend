b15952b464bdb2db24adc668fe5cd47e8cb9415e
eduardescola <edduardescola@gmail.com>
2025-05-13 17:11:11 +0200

    password xifrat

--- src/main/java/com/example/services/UserService.java
+++ src/main/java/com/example/services/UserService.java
@@ -1,24 +1,31 @@
 package com.example.services;
 
-import com.example.entities.User;
-import com.example.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.entities.User;
+import com.example.repositories.UserRepository;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public List<User> obtenerTodos() {
         return userRepository.findAll();
     }
 
     public User crear(User user) {
+        String passwordCifrada = passwordEncoder.encode(user.getPassword());
+        user.setPassword(passwordCifrada);
         return userRepository.save(user);
     }
 
